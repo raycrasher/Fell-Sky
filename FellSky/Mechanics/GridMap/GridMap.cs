b319f9c57797e9700376adaeb81f2220b3b466d2
Ian David Garcia <cinquedia@gmail.com>
2015-09-10 19:24:45 +0800

    GridMaps

---
+++ FellSky/Mechanics/GridMap/GridMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.Mechanics.Ground
+{
+    public enum GridDirection
+    {
+        E, SE, S, SW, W, NW, N, NE
+    }
+    public class GroundMap
+    {
+    }
+}
