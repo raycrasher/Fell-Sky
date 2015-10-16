4180cb6322b919539526e3682107cde0e5a3e0dd
Ian David Garcia <cinquedia@gmail.com>
2015-10-16 19:36:52 +0800

    After work commit

--- FellSky/SpaceScene/Systems/ShipUpdateSystem.cs
+++ FellSky/SpaceScene/Systems/ShipUpdateSystem.cs
@@ -5,12 +5,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Artemis;
+using FellSky.Mechanics.Ships;
 
 namespace FellSky.SpaceScene.Systems
 {
-    public class SpaceSceneShipUpdateSystem : Artemis.System.EntityComponentProcessingSystem<Ship>
+    public class ShipUpdateSystem : Artemis.System.EntityComponentProcessingSystem<Ship>
     {
         public override void Process(Entity entity, Ship ship)
+        {
+            UpdateHealth(entity, ship);
+            UpdateHeat(entity, ship);
+        }
+
+        private void UpdateHeat(Entity entity, Ship ship)
+        {
+            
+        }
+
+        private void UpdateHealth(Entity entity, Ship ship)
         {
             
         }
