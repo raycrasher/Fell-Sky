806256ba93a688eecf42cbde00a341804e9d6701
Ian David Garcia <cinquedia@gmail.com>
2015-11-02 18:32:31 +0800

    Add Rotate capability to MouseControlledTransformComponent.

--- FellSky/EntityComponents/MouseControlledTransformComponent.cs
+++ FellSky/EntityComponents/MouseControlledTransformComponent.cs
@@ -1,11 +1,14 @@
 ﻿using Artemis.Interface;
 using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 
 namespace FellSky.EntityComponents
 {
     public class MouseControlledTransformComponent: IComponent
     {
+        public Transform InitialTransform { get; set; }
         public Matrix TransformationMatrix { get; set; } = Matrix.Invert(Matrix.Identity);
+        public Vector2 InitialMousePosition { get; internal set; }
     }
 }
--- FellSky/Systems/MouseControlledTransformSystem.cs
+++ FellSky/Systems/MouseControlledTransformSystem.cs
@@ -12,11 +12,29 @@ using Artemis.Attributes;
 
 namespace FellSky.Systems
 {
+    public enum MouseControlledTransformMode
+    {
+        Translate = 1, Rotate = 2, Scale = 3
+    }
+
     [ArtemisEntitySystem(ExecutionType =Artemis.Manager.ExecutionType.Synchronous, GameLoopType =Artemis.Manager.GameLoopType.Update, Layer = 4)]
     public class MouseControlledTransformSystem : Artemis.System.EntityComponentProcessingSystem<MouseControlledTransformComponent, Transform>
     {
         private Camera2D _camera;
         private IMouseService _mouse;
+
+        public override void OnAdded(Entity entity) => GetInitialTransform(entity);
+        //public override void OnChange(Entity entity) => GetInitialTransform(entity);
+
+        public MouseControlledTransformMode Mode { get; set; } = MouseControlledTransformMode.Translate;
+
+        private void GetInitialTransform(Entity entity)
+        {
+            var xform = entity.GetComponent<Transform>();
+            var component = entity.GetComponent<MouseControlledTransformComponent>();
+            component.InitialTransform = xform.Clone();
+            component.InitialMousePosition = _camera.ScreenToCameraSpace(_mouse.ScreenPosition);
+        }
 
         public override void LoadContent()
         {
@@ -25,10 +43,29 @@ namespace FellSky.Systems
             _camera = BlackBoard.GetEntry<Camera2D>(Camera2D.PlayerCameraName);
             base.LoadContent();
         }
-        public override void Process(Entity entity, MouseControlledTransformComponent control, Transform xform)
+
+        public override void Process(Entity entity, MouseControlledTransformComponent control, Transform transform)
         {
-            var worldPosition = _camera.ScreenToCameraSpace(_mouse.ScreenPosition);
-            xform.Position = Vector2.Transform(worldPosition, control.TransformationMatrix);
+            var worldMousePos = _camera.ScreenToCameraSpace(_mouse.ScreenPosition);
+            switch (Mode)
+            {
+                case MouseControlledTransformMode.Translate:
+                    transform.Position = Vector2.Transform(worldMousePos, control.TransformationMatrix);
+                    break;
+                case MouseControlledTransformMode.Scale:
+                    break;
+                case MouseControlledTransformMode.Rotate:
+                    DoRotate(worldMousePos, control, transform);
+                    break;
+            }
+        }
+
+        private void DoRotate(Vector2 worldMousePos, MouseControlledTransformComponent control, Transform transform)
+        {
+            var offset = Vector2.Transform(worldMousePos, control.TransformationMatrix) - transform.Position;
+            var initialAngle = (control.InitialMousePosition - control.InitialTransform.Position).ToAngleRadians();
+
+            transform.Rotation = MathHelper.WrapAngle(control.InitialTransform.Rotation + initialAngle);
         }
     }
 }
