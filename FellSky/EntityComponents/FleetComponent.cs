3f4ec14de3b80674cca6376e334a8cb738798480
Ian David Garcia <cinquedia@gmail.com>
2015-11-09 19:31:59 +0800

    More changes to model/components

---
+++ FellSky/EntityComponents/FleetComponent.cs
@@ -0,0 +1,16 @@
+﻿using Artemis.Interface;
+using FellSky.Mechanics.Ships;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.EntityComponents
+{
+    public class FleetComponent: IComponent
+    {
+        public string Name { get; set; } = "Unknown Fleet";
+        public List<Ship> Ships { get; set; } = new List<Ship>();
+    }
+}
---
+++ FellSky/EntityComponents/SignalEmitterComponent.cs
@@ -0,0 +1,13 @@
+﻿using Artemis.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.EntityComponents
+{
+    public class SignalEmitterComponent: IComponent
+    {
+    }
+}
