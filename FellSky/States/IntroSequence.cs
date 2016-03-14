7b1397b4be25582e456299acc04d50b12d884a17
Ian David Garcia <cinquedia@gmail.com>
2016-03-14 17:16:59 +0800

    IntroSequence.cs

---
+++ FellSky/States/IntroSequence.cs
@@ -0,0 +1,25 @@
+﻿using FellSky.States;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.States { 
+    public class IntroSequence: GameState
+    {
+        public enum State
+        {
+            Start,
+            Intro,
+            Launch,
+        }
+
+        public bool SequenceDone { get; private set; }
+
+        public System.Collections.IEnumerable DoIntroSequence()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
