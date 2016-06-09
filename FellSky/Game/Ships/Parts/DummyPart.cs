72c24560fb81872c9cbe568690cdb58c45aa7511
Ian David Garcia <cinquedia@gmail.com>
2016-06-09 19:37:27 +0800

    Touched up editor.

---
+++ FellSky/Game/Ships/Parts/DummyPart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Artemis;
+
+namespace FellSky.Game.Ships.Parts
+{
+    public class DummyPart : ShipPart
+    {
+        public override Entity CreateEntity(EntityWorld world, Entity ship, int? index = default(int?))
+        {
+            // we don't create an entity, so return null.
+            return null;
+        }
+    }
+}
