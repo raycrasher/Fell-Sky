df891af7f4d9411d06e10c5c3c32fd00163949a4
Ian David Garcia <cinquedia@gmail.com>
2016-06-14 15:03:08 +0800

    Added AnimationEditorControl for editor.

---
+++ FellSky.Editor/PartAnimationEditorViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.Editor
+{
+    [PropertyChanged.ImplementPropertyChanged]
+    public class PartAnimationEditorViewModel
+    {
+        public float? PositionX { get; set; }
+        public float? PositionY { get; set; }
+        public float? ScaleX { get; set; }
+        public float? ScaleY { get; set; }
+        public float? Rotation { get; set; }
+
+        public float CurrentTime { get; set; }
+    }
+}
