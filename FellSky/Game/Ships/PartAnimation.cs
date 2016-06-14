2f49d5422a8767b7d3f82d78c6ce47db6bcbb614
Ian David Garcia <cinquedia@gmail.com>
2016-06-14 15:02:41 +0800

    Added PartAnimation model.

---
+++ FellSky/Game/Ships/PartAnimation.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.Game.Ships
+{
+    public class Keyframe<T>
+    {
+        public float Time { get; set; }
+        public T Value { get; set; }
+    }
+
+    public class PartAnimation
+    {
+        public List<Keyframe<Vector2>> Position { get; set; } = new List<Keyframe<Vector2>>();
+        public List<Keyframe<float>> Rotation { get; set; } = new List<Keyframe<float>>();
+        public List<Keyframe<Vector2>> Scale { get; set; } = new List<Keyframe<Vector2>>();
+        public List<Keyframe<Color>> Color { get; set; } = new List<Keyframe<Color>>();
+        public List<Keyframe<float>> Alpha { get; set; } = new List<Keyframe<float>>();
+
+        public void Sort()
+        {
+            Position.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+            Rotation.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+            Scale.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+            Color.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+            Alpha.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+        }
+    }
+}
