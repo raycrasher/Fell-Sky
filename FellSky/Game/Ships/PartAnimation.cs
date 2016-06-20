328ca261aab06d166618f8bf528758888cdb343f
Ian David Garcia <cinquedia@gmail.com>
2016-06-20 19:10:24 +0800

    Added implementation of keyframe based animation.

--- FellSky/Game/Ships/PartAnimation.cs
+++ FellSky/Game/Ships/PartAnimation.cs
@@ -17,49 +17,162 @@ namespace FellSky.Game.Ships
 
         public Keyframe() { }
         public float Time { get; set; }
+        
+        /// <summary>
+        /// The value, relative to the previous keyframe.
+        /// </summary>
         public T Value { get; set; }
     }
 
     public class PartAnimation
     {
-        public List<Keyframe<Vector2>> Position { get; set; } = new List<Keyframe<Vector2>>();
-        public List<Keyframe<float>> Rotation { get; set; } = new List<Keyframe<float>>();
-        public List<Keyframe<Vector2>> Scale { get; set; } = new List<Keyframe<Vector2>>();
-        public List<Keyframe<Color>> Color { get; set; } = new List<Keyframe<Color>>();
-        public List<Keyframe<float>> Alpha { get; set; } = new List<Keyframe<float>>();
+        public List<Keyframe<Vector2>> Positions { get; set; } = new List<Keyframe<Vector2>>();
+        public List<Keyframe<float>> Rotations { get; set; } = new List<Keyframe<float>>();
+        public List<Keyframe<Vector2>> Scales { get; set; } = new List<Keyframe<Vector2>>();
+        public List<Keyframe<Color>> Colors { get; set; } = new List<Keyframe<Color>>();
+        public List<Keyframe<float>> Alphas { get; set; } = new List<Keyframe<float>>();
+
+        public IEnumerable<Vector2> AnimatePosition(Func<float> deltaTimeFunction)
+            => Animate(Positions, Vector2.Lerp, deltaTimeFunction, Vector2.Zero);
+
+        public IEnumerable<float> AnimateRotation(Func<float> deltaTimeFunction)
+            => Animate(Rotations, MathHelper.Lerp, deltaTimeFunction, 0);
+
+        public IEnumerable<Vector2> AnimateScale(Func<float> deltaTimeFunction)
+            => Animate(Scales, Vector2.Lerp, deltaTimeFunction, Vector2.One);
+
+        public IEnumerable<Color> AnimateColor(Func<float> deltaTimeFunction)
+            => Animate(Colors, Color.Lerp, deltaTimeFunction, Color.White);
+
+        public IEnumerable<float> AnimateAlpha(Func<float> deltaTimeFunction)
+            => Animate(Alphas, MathHelper.Lerp, deltaTimeFunction, 1);
 
         public void Sort()
         {
-            Position.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
-            Rotation.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
-            Scale.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
-            Color.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
-            Alpha.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+            Positions.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+            Rotations.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+            Scales.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+            Colors.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+            Alphas.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
         }
 
-        public void AddPositionKeyframe(float percentage)
+        public void AddPositionKeyframe(float time, Vector2? value = null)
         {
-            Position.Add(new Keyframe<Vector2>(percentage, GetPosition(percentage)));
-            Position.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+            Positions.Add(new Keyframe<Vector2>(time, value ?? GetPosition(time)));
+            Positions.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
         }
 
-        private Vector2 GetPosition(Vector2 initial, float percentage)
+        public void AddRotationKeyframe(float time, float? value = null)
         {
-            foreach(var item in Position)
+            Rotations.Add(new Keyframe<float>(time, value ?? GetRotation(time)));
+            Rotations.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+        }
+
+        public void AddScaleKeyframe(float time, Vector2? value = null)
+        {
+            Scales.Add(new Keyframe<Vector2>(time, value ?? GetScale(time)));
+            Scales.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+        }
+
+        public void AddColorKeyframe(float time, Color? value = null)
+        {
+            Colors.Add(new Keyframe<Color>(time, value ?? GetColor(time)));
+            Colors.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+        }
+
+        public void AddAlphaKeyframe(float time, float? value = null)
+        {
+            Alphas.Add(new Keyframe<float>(time, value ?? GetAlpha(time)));
+            Alphas.Sort((a, b) => Comparer<float>.Default.Compare(a.Time, b.Time));
+        }
+
+        public Vector2 GetPosition(float time)
+            => GetValue(Positions, Vector2.Lerp, time, Vector2.Zero);
+
+        public float GetRotation(float time)
+            => GetValue(Rotations, MathHelper.Lerp, time, 0);
+
+        public Vector2 GetScale(float time)
+            => GetValue(Scales, Vector2.Lerp, time, Vector2.One);
+
+        public Color GetColor(float time)
+            => GetValue(Colors, Color.Lerp, time, Color.White);
+
+        public float GetAlpha(float time)
+            => GetValue(Alphas, MathHelper.Lerp, time, 1f);
+
+
+        public static IEnumerable<T> Animate<T>(List<Keyframe<T>> frames, Func<T, T, float, T> lerpFunction, Func<float> deltaTimeFunction, T defaultValue)
+        {
+            float time = 0;
+            float end = frames.Count <= 0 ? 0 : frames[frames.Count - 1].Time;
+
+            // no keyframe case
+            if (frames.Count <= 0)
             {
-
+                yield return defaultValue;
+                yield break;
             }
+
+            // one keyframe case
+            if (frames.Count == 1)
+            {
+                var keyframe = frames[0];
+                while (time < end)
+                {
+                    yield return lerpFunction(defaultValue, keyframe.Value, time / keyframe.Time);
+                    time += deltaTimeFunction();
+                }
+                yield return keyframe.Value;
+                yield break;
+            }
+
+            // many keyframes case
+            int index = 0;
+            Keyframe<T> currentFrame = frames[index];
+            T lastValue = defaultValue;
+            float lastTime = 0f;
+
+            while (index < frames.Count - 1)
+            {
+                yield return lerpFunction(lastValue, currentFrame.Value, (time - lastTime) / currentFrame.Time);
+                time += deltaTimeFunction();
+
+                if (time > frames[index + 1].Time)
+                {
+                    lastValue = currentFrame.Value;
+                    lastTime = currentFrame.Time;
+                    index++;
+                    currentFrame = frames[index];
+                }
+            }
+            yield return currentFrame.Value;
         }
 
-        private int GetLastIndex<T>(List<Keyframe<T>> list, float percentage)
+        public static T GetValue<T>(List<Keyframe<T>> frames, Func<T, T, float, T> lerpFunction, float time, T defaultValue)
         {
-            int index;
+            if (frames.Count <= 0) return defaultValue;
 
-            for(index = 0; index < list.Count; index++)
+            int i;
+            Keyframe<T> last = null, current = null;
+
+            for (i = 0; i < frames.Count; i++)
             {
-
+                last = current;
+                current = frames[i];
+                if (frames[i].Time < time) break;
             }
-            return index;
+
+            if (i >= frames.Count)
+                return current.Value;
+            else if (last == null)
+            {
+                return lerpFunction(defaultValue, current.Value, time / current.Time);
+            }
+            else
+            {
+                return lerpFunction(last.Value, current.Value, time / current.Time);
+            }
         }
     }
 }
