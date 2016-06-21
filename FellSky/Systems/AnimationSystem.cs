5eff5c2797e4b0588bb29e0da08da4d8866f4635
Ian David Garcia <cinquedia@gmail.com>
2016-06-21 19:29:57 +0800

    Added AnimationComponent and AnimationSystem.

--- FellSky/Systems/AnimationSystem.cs
+++ FellSky/Systems/AnimationSystem.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Artemis;
+using FellSky.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,105 +8,24 @@ using System.Threading.Tasks;
 
 namespace FellSky.Systems
 {
-    class AnimationSystem
+    public class AnimationSystem: Artemis.System.EntitySystem
     {
-        /*
-            EVENT eventname
-	            ROTATE partid degrees speed
-	            TRANSLATE partid x y
-	            START MOTOR partid
-                STOP MOTOR partid
-	            RESET POSITION partid speed
-	            RESET ROTATION partid speed
-	            COLOR partid speed r g b a
-	            COLOR partid speed r g b
-	            COLOR partid speed a
-	            FIRE eventname
-        */
-
-        enum CommandType
+        public AnimationSystem()
+            : base(Aspect.All(typeof(AnimationComponent)))
         {
-            Rotate, Translate, Lock, ResetPosition, ResetRotation, ColorRgba, ColorA, ColorRgb, FireEvent
         }
 
-        class Script
+        protected override void ProcessEntities(IDictionary<int, Entity> entities)
         {
-            public string EventId;
-            public List<ScriptCommand> Commands = new List<ScriptCommand>();
-
-            public Script(string evt)
+            foreach(var entity in entities.Values)
             {
-                EventId = evt;
+                var animation = entity.GetComponent<AnimationComponent>();
+                animation.Position?.MoveNext();
+                animation.Rotation?.MoveNext();
+                animation.Scale?.MoveNext();
+                animation.Color?.MoveNext();
+                animation.Alpha?.MoveNext();
             }
         }
-
-        class ScriptCommand
-        {
-            public ScriptCommand(CommandType type, params object[] parameters)
-            {
-                Command = type;
-                Parameters = parameters;
-            }
-            public CommandType Command;
-            public object[] Parameters;
-        }
-
-        void Parse(string script)
-        {
-            var lines = script.Split('\n');
-
-            Script currentScript = null;
-            List<Script> scripts = new List<Script>();
-
-            foreach (var line in lines)
-            {
-                var items = line.Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
-                if (items.Length <= 0) continue;
-
-
-                if (items[0].Equals("event", StringComparison.InvariantCultureIgnoreCase))
-                {
-                    currentScript = new Script(items[1]);
-                    scripts.Add(currentScript);
-                }
-                else if (currentScript != null)
-                {                
-                    if (items[0].Equals("rotate", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        currentScript.Commands.Add(new ScriptCommand(CommandType.Rotate, items[1], float.Parse(items[2]), float.Parse(items[3])));
-                    }
-                    else if (items[0].Equals("translate", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        currentScript.Commands.Add(new ScriptCommand(CommandType.Translate, items[1], float.Parse(items[2]), float.Parse(items[3])));
-                    }
-                    else if (items[0].Equals("lock", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        currentScript.Commands.Add(new ScriptCommand(CommandType.Translate, items[1], float.Parse(items[2]), float.Parse(items[3])));
-                    }
-                    else if (items[0].Equals("reset", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        currentScript.Commands.Add(new ScriptCommand(CommandType.Translate, items[1], float.Parse(items[2]), float.Parse(items[3])));
-                    }
-                    else if (items[0].Equals("color", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        switch (items.Length)
-                        {
-                            case 4:
-                                break;
-                            case 6:
-                                break;
-                            case 7:
-                                break;
-                        }
-
-                    }
-                    else if (items[0].Equals("fire", StringComparison.InvariantCultureIgnoreCase))
-                    {
-
-                    }
-                }
-            }
-        }
-        
     }
 }
