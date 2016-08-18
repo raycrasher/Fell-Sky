8cf68a7f462ea767ca857b491dd58b4692431182
Ian David Garcia <cinquedia@gmail.com>
2016-08-18 19:33:53 +0800

    Daily commit

---
+++ FellSky/Components/FrameAnimationComponent.cs
@@ -0,0 +1,21 @@
+﻿using Artemis.Interface;
+using FellSky.Framework;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.Components
+{
+    public class FrameAnimationComponent: IComponent
+    {
+        public int CurrentFrame => MathHelper.Clamp((int)(CurrentTime / Fps), 0, Frames?.Count ?? 0);
+
+        public float Fps;
+        public float CurrentTime;
+        public List<SpriteComponent> Frames = new List<SpriteComponent>();
+        public AnimationState State = AnimationState.Stopped;
+    }
+}
---
+++ FellSky/Systems/FrameAnimationSystem.cs
@@ -0,0 +1,23 @@
+﻿using Artemis;
+using FellSky.Components;
+using FellSky.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.Systems
+{
+    public class FrameAnimationSystem : Artemis.System.EntityComponentProcessingSystem<FrameAnimationComponent, SpriteComponent>
+    {
+        public override void Process(Entity entity, FrameAnimationComponent animation, SpriteComponent sprite)
+        {
+            if (animation.State == Framework.AnimationState.Playing)
+            {
+                animation.CurrentTime += EntityWorld.Delta * 0.001f;
+                entity.AddComponent(animation.Frames[animation.CurrentFrame]);
+            }            
+        }
+    }
+}
