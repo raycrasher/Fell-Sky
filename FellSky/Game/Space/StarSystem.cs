a5d2bf2281d68823401789e670b248010695cfcb
Ian David Garcia <cinquedia@gmail.com>
2016-08-31 18:19:37 +0800

    Daily commit

---
+++ FellSky/Game/Space/StarSystem.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.Game.Space
+{
+    public class StarSystem
+    {
+        public string Name { get; set; }
+        public Vector2 Position { get; set; }
+
+        [Newtonsoft.Json.JsonIgnore]
+        public SpaceObject PrimaryObject => Objects[0];
+
+        public List<SpaceObject> Objects { get; set; }
+    }
+}
