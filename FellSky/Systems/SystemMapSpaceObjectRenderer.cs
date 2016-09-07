5782f0278d264a1a97b8cad45b04ebdbd39fa288
Ian David Garcia <cinquedia@gmail.com>
2016-09-08 00:40:49 +0800

    Semi-working planet renderer. Needs more high-res planet UV-sphere

--- FellSky/Systems/SystemMapSpaceObjectRenderer.cs
+++ FellSky/Systems/SystemMapSpaceObjectRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using Artemis;
+using FellSky.Framework;
 using FellSky.Components;
 using FellSky.Game.Space;
 using FellSky.Services;
@@ -21,6 +22,11 @@ namespace FellSky.Systems
         private SpriteComponent _planetShadowMask;
         private Transform _xform = new Transform();
 
+        private Model _sphere;
+        private Camera _camera;
+        private Matrix _projectionMatrix;
+        
+
         //private Model _sphereMesh;
 
         public SystemMapSpaceObjectRenderer()
@@ -33,14 +39,35 @@ namespace FellSky.Systems
 
         public override void LoadContent()
         {
+            
             //_sphereMesh = _content.Load<Model>("Meshes/Sphere");
             _planetShadowMask = ServiceLocator.Instance.GetService<ISpriteManagerService>().CreateSpriteComponent("planetshadowmask");
+            _projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.ToRadians(20.0f), GameEngine.Instance.GraphicsDevice.Viewport.AspectRatio,
+                1.0f, 10000.0f);
+
+            _sphere = _content.Load<Model>("Meshes/uvsphere");
+
+            var sunEffect = (BasicEffect)_sphere.Meshes[0].Effects[0];
+
+            sunEffect.World = Matrix.Identity;
+            //sunEffect.View = Matrix.CreateLookAt(new Vector3(_camera.Transform.Position, 0.5f), new Vector3(_camera.Transform.Position, 0), Vector3.UnitY);
+            sunEffect.Projection = _projectionMatrix;
+
+            sunEffect.LightingEnabled = true;
+            sunEffect.Texture = _content.Load<Texture2D>("Textures/spaceobjects/sun");
+            sunEffect.TextureEnabled = true;
+            sunEffect.AmbientLightColor = Vector3.Zero;
+            sunEffect.EmissiveColor = Vector3.One;
+            sunEffect.SpecularPower = 0;
+            sunEffect.DiffuseColor = Vector3.Zero;
         }
 
         protected override void ProcessEntities(IDictionary<int, Entity> entities)
         {
-            var camera = EntityWorld.GetActiveCamera();
-            _spriteBatch.Begin(transformMatrix: camera.GetViewMatrix(1.0f), samplerState: SamplerState.AnisotropicClamp);
+            _device.SamplerStates[0] = SamplerState.AnisotropicWrap;
+            _camera = EntityWorld.GetActiveCamera();
+            //_spriteBatch.Begin(transformMatrix: camera.GetViewMatrix(1.0f), samplerState: SamplerState.AnisotropicClamp);
             foreach(var entity in entities.Values)
             {
                 var spaceObjectComponent = entity.GetComponent<SpaceObjectComponent>();
@@ -49,16 +76,36 @@ namespace FellSky.Systems
                 else if (spaceObjectComponent.Object is Star)
                     DrawStar(entity, (Star)spaceObjectComponent.Object);
             }
-            _spriteBatch.End();
+            //_spriteBatch.End();
         }
 
         private void DrawStar(Entity entity, Star star)
         {
+            /*
             var sprite = entity.GetComponent<SpriteComponent>();
             var xform = entity.GetComponent<Transform>();
             sprite.Draw(_spriteBatch, xform);
+            */
+
+            var xform = entity.GetComponent<Transform>();
+
+            var sunEffect = (BasicEffect)_sphere.Meshes[0].Effects[0];
+
+            var inverse = new Vector2(1, -1) * 0.01f;
+
+            sunEffect.World = Matrix.CreateTranslation(new Vector3(xform.Position, 0)) * Matrix.CreateRotationX(MathHelper.ToRadians(80)) * Matrix.CreateScale(1f);
+            sunEffect.View = Matrix.CreateLookAt(new Vector3(_camera.Transform.Position * inverse, 20 + _camera.Zoom ), new Vector3(_camera.Transform.Position * inverse, 0), Vector3.UnitY);
+                        
+            _sphere.Meshes[0].Draw();
+            
         }
 
+        private void DrawPlanet(Entity entity, Planet planet)
+        {
+
+        }
+
+        /*
         private void DrawPlanet(Entity entity, Planet planet)
         {
             var sprite = entity.GetComponent<SpriteComponent>();
@@ -68,5 +115,6 @@ namespace FellSky.Systems
             sprite.Draw(_spriteBatch, _xform);
             _planetShadowMask.Draw(_spriteBatch, _xform);
         }
+        */
     }
 }
