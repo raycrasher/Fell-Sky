5cc3383678e968c63febcaac112d417fc35787b0
Ian David Garcia <cinquedia@gmail.com>
2016-10-14 18:47:21 +0800

    Updated packages. Added Explosion system and component.

---
+++ FellSky/Components/ExplosionComponent.cs
@@ -0,0 +1,36 @@
+﻿using Artemis.Interface;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.Components
+{
+    public class ExplosionComponent: IComponent
+    {
+        /// <summary>
+        /// Add blast wave effect
+        /// </summary>
+        public bool BlastWave { get; set; } 
+        public float BlastWaveSize { get; set; }
+        public int BlastWaveDurationMs { get; set; }
+        public Color BlastWaveColor { get; set; }
+        public SpriteComponent BlastWaveSprite { get; set; }
+
+        public bool Fireball { get; set; }
+        public float FireballStartSize { get; set; }
+        public float FireballEndSize { get; set; }
+        public int FireballDurationMs { get; set; }
+        public Color FireballColor { get; set; }
+        public SpriteComponent FireballSprite { get; set; }
+
+        public bool Smoke { get; set; }
+        public int SmokeNumParticles { get; set; }
+        public int SmokeDuration { get; set; }
+        public SpriteComponent SmokeSprite { get; set; }
+
+        public float ScreenShake { get; set; }
+    }
+}
---
+++ FellSky/Systems/ExplosionSystem.cs
@@ -0,0 +1,38 @@
+﻿using Artemis;
+using Artemis.Manager;
+using FellSky.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.Systems
+{
+    public class ExplosionSystem: Artemis.System.EntitySystem
+    {
+    }
+
+    public class ExplosionRenderer : Artemis.System.EntitySystem
+    {
+        public ExplosionRenderer()
+            : base(Aspect.All(typeof(ExplosionComponent)))
+        {
+        }
+
+        private void DrawFireball(Entity entity, ExplosionComponent explosion)
+        {
+
+        }
+
+        private void DrawBlastWave(Entity entity, ExplosionComponent explosion)
+        {
+
+        }
+
+        private void DoScreenShake(Entity entity, ExplosionComponent explosion)
+        {
+
+        }
+    }
+}
