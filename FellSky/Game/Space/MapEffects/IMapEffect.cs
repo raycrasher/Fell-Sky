5407f194e9c13ea49d0a24b7e4796bbe9aec1a55
Ian David Garcia <cinquedia@gmail.com>
2016-10-19 18:26:01 +0800

    Added mapping and map features

---
+++ FellSky/Game/Space/MapEffects/IMapEffect.cs
@@ -0,0 +1,18 @@
+﻿using Artemis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.Game.Space.MapEffects
+{
+    public interface IMapEffect
+    {
+        string Name { get; }
+        string IconId { get; }
+        string Description { get; }
+
+        void ApplyToMap(SpaceMap map, EntityWorld world);
+    }
+}
